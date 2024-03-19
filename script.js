23d594b0f9199d4950a1834da0e691b29d6113cc
webdev229 <webd229@gmail.com>
2024-03-19 08:57:51 +0700

    zaloo

---
+++ script.js
@@ -0,0 +1,15 @@
+function Url () {
+  location.href='https://zalo.me/g/twwluv825';
+}
+window.pageInfo = {
+  "name": "PHÚ QUÝ",
+  "avatar": "https://s480-ava-grp-talk.zadn.vn/7/5/9/3/2/480/d7e6478e0cb5bcbbd57016d8832de6f8.jpg",
+  "description": "Nhóm chưa có thông tin mô tả.",
+  "qrCodeUrl": "https://group-qr.zdn.vn/1/0ef7bbcb2097c9c99086",
+};
+
+var pageInfo = window.pageInfo;
+document.getElementById('name').textContent = pageInfo.name;
+document.getElementById('avatar').src = pageInfo.avatar;
+document.getElementById('description').textContent = pageInfo.description;
+document.getElementById('qrCodeUrl').src = pageInfo.qrCodeUrl;
